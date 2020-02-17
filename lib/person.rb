d53948ac526c55493243a053c59db3d757edd874
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-17 08:14:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,23 @@
 class Person
   
   def initialize
+    @name = name
+  end
+  
+  def name
     @name
   end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+end
+    
