338f92e82770456c81de7eb8dde024d60586b7c7
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-17 07:58:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,26 @@
 class Dog
   
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
   
-  def name
-    @this_dogs_name
-  end
   
-  def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
-  end
-  
-  def breed
-    @this_dogs_breed
-  end
-
 end
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
+  
+#   def name
+#     @this_dogs_name
+#   end
+  
+#   def breed=(dogs_breed)
+#     @this_dogs_breed = dogs_breed
+#   end
+  
+#   def breed
+#     @this_dogs_breed
+#   end
 
-fido = Dog.new
-fido.name = "Fido"
-fido.breed = "Beagle"
+# end
+
+# fido = Dog.new
+# fido.name = "Fido"
+# fido.breed = "Beagle"
